aed35833e29fc92f86f7eb7147acf6206d2c6666
MenemenDevs <menemensogantutmuyor@gmail.com>
2025-10-25 22:39:31 +0300

    late night comminting (please teach me how to enable dark mode in sourcetree

--- Image.py
+++ Image.py
@@ -3,10 +3,20 @@ from PIL import Image
 import random
 import string
 
+select = input("size? " \
+"1. 1080x1920" \
+"2. 1920x1080" \
+"3. 720x1280" \
+"4. 1280x720" \
+"5. 1440x2560" \
+"6. 2560x1440 " \
+)
+times = int(input("how many images? "))
+#THIS SHOULD GENERATE RANDOM IMAGE NAME TYPA SHT U KNOW
 def random_string(length=83):
     chars = string.ascii_letters + string.digits
     return ''.join(random.choice(chars) for _ in range(length))
-
+# THIS SHT GENERATES RANDOM IMAGE WITH RANDOM THINGS ON IT (Maybe u get the sf-90 motor photo idk )
 def generate_random_image(width, height):
     
     array = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
@@ -18,10 +28,16 @@ def generate_random_image(width, height):
     img.save(filename)
     print(f"{filename}")
 
- 
-generate_random_image(1080, 1920)
-generate_random_image(1920, 1080)
-generate_random_image(720, 1280)
-generate_random_image(1280, 720)
-generate_random_image(1440, 2560)
-generate_random_image(2560, 1440)
+for _ in range(times):
+    if select == "1":
+        generate_random_image(1080, 1920)
+    if select == "2":
+        generate_random_image(1920, 1080)
+    if select == "3":
+        generate_random_image(720, 1280)
+    if select == "4":
+        generate_random_image(1280, 720)
+    if select == "5":
+        generate_random_image(1440, 2560)
+    if select == "6":
+        generate_random_image(2560, 1440)
